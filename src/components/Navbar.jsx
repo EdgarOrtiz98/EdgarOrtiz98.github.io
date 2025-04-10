4f62a1341f3882018a9401fc1b4004b774d85fbd
EdgarOrtiz98 <123284231+EdgarOrtiz98@users.noreply.github.com>
2025-04-09 20:48:03 -0600

    Add Fontello icon font files and SVG definitions
    - Added fontello.svg containing the SVG definitions for the Fontello icon font.
    - Added fontello.ttf, fontello.woff, and fontello.woff2 files for the Fontello icon font in various formats.
    - These files include the necessary glyphs and metadata for proper rendering of icons in the application.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Navbar.css';
 import Logo from '../assets/images/Logo.png';
+import '../assets/fontello/fontello.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo-container">
-				<img src={Logo} alt="Logo" className="logo"/>
+        <img src={Logo} alt="Logo" className="logo"/>
       </div>
-      <ul className="nav-links">
+
+      <button 
+        className="menu-toggle" 
+        onClick={toggleMenu}
+        aria-label="Toggle menu">
+        <i className="icon-menu"></i>
+      </button>
+
+      <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
         <li><a href="#">Inicio</a></li>
         <li><a href="#">Sobre mí</a></li>
         <li><a href="#">Contacto</a></li>
