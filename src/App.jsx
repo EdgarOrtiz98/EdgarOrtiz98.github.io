7c01eab506f174ce61400a94a267e89ef3e7bc91
EdgarOrtiz98 <123284231+EdgarOrtiz98@users.noreply.github.com>
2025-04-13 18:24:12 -0600

    feat: agregar sección de contacto con enlaces a redes sociales y estilos correspondientes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import "./styles/App.css";
 import "./styles/Home.css";
 import "./styles/About.css";
+import "./styles/Contact.css";
 import Navbar from "./components/Navbar.jsx";
 import SkillCard from "./components/SkillCard.jsx";
 import edgarImg from "./assets/images/EdgarDev.png";
+import GithubIcon from "./assets/images/github-icon.svg";
+import FacebookIcon from "./assets/images/facebook-icon.svg";
+import InstagramIcon from "./assets/images/instagram-Icon.svg";
+import YoutubeIcon from "./assets/images/youtube-icon.svg";
 
 function App() {
   return (
@@ -79,6 +84,72 @@ function App() {
       
       {/* Sección de contacto */}
       <div className="contact-content" id="contact">
+        <h2 className="contact-title">Contactame </h2>
+        <div className="contact-details">
+          <div className="contact-text">
+        <h3>¡Hablemos!</h3>
+        </div>
+        <div className="contact-text">
+          <p>
+            Si tienes alguna pregunta, inquietud o simplemente quieres
+            charlar, no dudes en ponerte en contacto conmigo. Estoy aquí para
+            ayudarte y colaborar en proyectos interesantes.
+          </p>
+        </div>
+          <div className="contact-form">
+            <a href="mailto:edchicort@gmail.com">
+              <button className="email-btn">
+                Enviame un correo
+              </button>
+            </a>
+            </div>
+          </div>
+            <h3>Sígueme en:</h3>
+          <div className="social-container">
+  {/* GitHub */}
+  <div className="tooltip github-tooltip">
+    <a href="https://github.com/tuusuario" target="_blank" rel="noopener">
+      <button className="github-btn">
+        <img src={GithubIcon} alt="GitHub" className="social-icon"/>
+        GitHub
+      </button>
+      <span className="tooltiptext">Visita mi GitHub</span>
+    </a>
+  </div>
+
+  {/* Facebook */}
+  <div className="tooltip facebook-tooltip">
+    <a href="https://facebook.com/tuusuario" target="_blank" rel="noopener">
+      <button className="facebook-btn">
+        <img src={FacebookIcon} alt="Facebook" className="social-icon"/>
+        Facebook
+      </button>
+      <span className="tooltiptext">Sígueme en Facebook</span>
+    </a>
+  </div>
+
+  {/* Instagram */}
+  <div className="tooltip instagram-tooltip">
+    <a href="https://instagram.com/tuusuario" target="_blank" rel="noopener">
+      <button className="instagram-btn">
+        <img src={InstagramIcon} alt="Instagram" className="social-icon"/>
+        Instagram
+      </button>
+      <span className="tooltiptext">Mis fotos en Instagram</span>
+    </a>
+  </div>
+
+  {/* YouTube */}
+  <div className="tooltip youtube-tooltip">
+    <a href="https://youtube.com/tuusuario" target="_blank" rel="noopener">
+      <button className="youtube-btn">
+        <img src={YoutubeIcon} alt="YouTube" className="social-icon"/>
+        YouTube
+      </button>
+      <span className="tooltiptext">Suscríbete en YouTube</span>
+    </a>
+  </div>
+</div>
       </div>
 
       {/* Sección de footer */}
