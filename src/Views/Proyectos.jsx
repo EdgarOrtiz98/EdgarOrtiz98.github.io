5d58d8f310d9e40ca73fdfa7a8fae63caccb8ff3
EdgarOrtiz98 <123284231+EdgarOrtiz98@users.noreply.github.com>
2025-04-25 19:04:53 -0600

    Refactor code structure for improved readability and maintainability

--- src/Views/Proyectos.jsx
+++ src/Views/Proyectos.jsx
@@ -1,6 +1,20 @@
 import Navbar from "../components/Navbar.jsx";
 import "../styles/Proyectos.css";
 import CardProyect from "../components/ProyectCard.jsx";
+import JuegoIngles from "../assets/images/Proyect-JuegoIngles.png";
+import html from "../assets/Tech-Icons/html5-original.svg";
+import css from "../assets/Tech-Icons/css3-original.svg";
+import javascript from "../assets/Tech-Icons/javascript-original.svg";
+import python from "../assets/Tech-Icons/python-original.svg";
+// import react from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg";
+// import nodejs from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg";
+// import java from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg";
+// import php from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg";
+// import laravel from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg";
+// import mysql from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg";
+// import mongodb from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg";
+// import git from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg";
+
 
 function Proyectos() {
   return (
@@ -8,9 +22,15 @@ function Proyectos() {
       <Navbar />
       <div className="card-container">
         <CardProyect
-          title="Proyecto 1"
-          description="Descripción del proyecto 1"
-          imageUrl="/screenshot-about.jpg"
+          title="Juego de Presente Perfecto (Ingles)"
+          description="El juego de presente perfecto ayuda a los usuarios a mejorar su conocimiento del presente perfecto en inglés. Los jugadores deben completar las oraciones seleccionando la opción correcta. El juego incluye preguntas con respuestas positivas, negativas y de interrogación."
+          imageUrl={JuegoIngles}
+          technologies={[
+            { name: "Html", icon: html },
+            { name: "Css", icon: css },
+            { name: "JavaScript", icon: javascript },
+            { name: "Python", icon: python },
+          ]}
         />
         <CardProyect
           title="Proyecto 2"
--- src/components/ProyectCard.jsx
+++ src/components/ProyectCard.jsx
@@ -1,14 +1,23 @@
 import "../styles/ProyectCard.css";
 
-const ProjectCard = ({ imageUrl, title, description }) => {
+const ProjectCard = ({ imageUrl, title, description, technologies = [] }) => {
   return (
     <div className="project-card">
       <div className="card-image">
         <img src={imageUrl} alt="Project screenshot" />
       </div>
       <div className="card-content">
-        <h5 className="card-title">{title}</h5>
+        <h3 className="card-title">{title}</h3>
         <p className="card-text">{description}</p>
+        <h4 className="card-technologies-title">Tecnologías usadas:</h4>
+        <div className="card-technologies">
+          {technologies.map((tech, index) => (
+            <div key={index} className="tech-item">
+              <img src={tech.icon} alt={tech.name} className="tech-icon" />
+              <span className="tech-name">{tech.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
