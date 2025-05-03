070ea847664dea74fdb955b3f5b676dd852a1bc9
EdgarOrtiz98 <123284231+EdgarOrtiz98@users.noreply.github.com>
2025-05-02 22:02:30 -0600

    add new CryptoCoin project

--- src/Views/Proyectos.jsx
+++ src/Views/Proyectos.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../components/Navbar.jsx";
 import "../styles/Proyectos.css";
 import CardProyect from "../components/ProyectCard.jsx";
+// imagenes de proyectos
 import JuegoIngles from "../assets/images/Proyect-JuegoIngles.png";
+import CriptoCoin from "../assets/images/Proyect-CriptoCoin.png";
+// iconos de tecnologias de desarrollo
 import html from "../assets/Tech-Icons/html5-original.svg";
 import css from "../assets/Tech-Icons/css3-original.svg";
 import javascript from "../assets/Tech-Icons/javascript-original.svg";
 import python from "../assets/Tech-Icons/python-original.svg";
-// import react from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg";
+import react from "../assets/Tech-Icons/react-original.svg";
 // import nodejs from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg";
 // import java from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg";
 // import php from "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg";
@@ -33,9 +36,15 @@ function Proyectos() {
           ]}
         />
         <CardProyect
-          title="Proyecto 2"
-          description="Descripción del proyecto 2"
-          imageUrl="/screenshot-home.jpg"
+          title="CryptoCoin Explorer"
+          description="Es una aplicación intuitiva que muestra datos esenciales del mercado de criptomonedas en tiempo real, incluyendo precios, cambios porcentuales y capitalización de mercado. Diseñada para inversores y entusiastas, permite filtrar por moneda fiduciaria y ofrece una vista clara de los activos más relevantes del mercado. (Implementa un API)"
+          imageUrl={CriptoCoin}
+          technologies={[
+            { name: "Html", icon: html },
+            { name: "Css", icon: css },
+            { name: "JavaScript", icon: javascript },
+            { name: "React", icon: react },
+          ]}
         />
         <CardProyect
           title="Proyecto 3"
